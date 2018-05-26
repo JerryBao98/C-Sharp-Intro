b82adb945ba1d5fceff3a93c22aa966080f2ff01
Jerry_Bao <jerrybao1998@outlook.com>
2018-05-25 21:24:30 -0600

    Added in dicitonary

--- intro.cs
+++ intro.cs
@@ -1,9 +1,32 @@
-namespace Name{
-    public class HTTPCookie{
+
+
+namespace Name
+{
+    public class HTTPCookie
+    {
+        //keys and values are both of type string
+        private Dictionary<string, string> _dictionary
+
+        public HTTPCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+
+        public string this[string key]
+        {
+            get {
+                return _dictionary[key];
+            }
+            set {
+                _dictionary[key] = value;
+            }
+        }
 
     }
-    class Program{
-        static void Main(string[] args) {  
+    class Program
+    {
+        static void Main(string[] args) 
+        {  
         }
     }
 }
