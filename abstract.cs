eb66a1286d7a0fefc85e8701e336f25811e2b21f
Jerry_Bao <jerrybao1998@outlook.com>
2018-06-03 15:43:30 -0600

    Implemented an abstract base class

--- abstract.cs
+++ abstract.cs
@@ -12,3 +12,20 @@ class circle:shape{
     }
 
 }
+
+
+//Here is an abstract implementation of the same agorithm
+public abstract class shape{
+    public int size { get; set; }   
+    //Doesnt make sense for a shape to be drawn, use abstraction to clarify it must be implemented later
+    public abstract void Draw{
+
+    }
+}
+
+class circle:shape{
+    public override void Draw{
+        System.Console.WriteLine("I am drawing a circle");
+    }
+
+}
