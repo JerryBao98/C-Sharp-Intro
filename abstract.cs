dd83c485f40fb35d2fdccd9b3e770099c92c8346
Jerry_Bao <jerrybao1998@outlook.com>
2018-06-03 15:46:50 -0600

    Added in more useful comments

--- abstract.cs
+++ abstract.cs
@@ -18,12 +18,16 @@ class circle:shape{
 public abstract class shape{
     public int size { get; set; }   
     //Doesnt make sense for a shape to be drawn, use abstraction to clarify it must be implemented later
+    //Cannot include implementation, but only a declaration, if a method is abstract, so is the class
+    //The derived class must override all the abstractions
+    //Abstract classes cannot be initialized
     public abstract void Draw{
 
     }
 }
 
 class circle:shape{
+    // Note the override
     public override void Draw{
         System.Console.WriteLine("I am drawing a circle");
     }
