19293f63247221026dd32fa710ebca748c458f10
Jerry_Bao <jerrybao1998@outlook.com>
2018-06-03 15:40:10 -0600

    Added in a circle object

--- abstract.cs
+++ abstract.cs
@@ -1,5 +1,4 @@
-class shape
-{
+class shape{
     public int size { get; set; }   
     //Doesnt make sense for a shape to be drawn, use abstraction to clarify it must be implemented later
     public virtual void Draw{
@@ -7,6 +6,9 @@ class shape
     }
 }
 
-class shape{
+class circle:shape{
+    public override void Draw{
+        System.Console.WriteLine("I am drawing a circle");
+    }
 
 }
